b1c092e319264afec9bc65893f05069afc07f1ab
Cam Zarubiak <cameronz@telus.net>
2018-03-03 14:46:37 -0800

    online

--- app/main.py
+++ app/main.py
@@ -135,7 +135,7 @@ def move():
 		    #if [j, height - 2] in list:
 		        #priority.append(['d', 400])
 		    if snakehead_x == width - 1:
-		   	    priority.append(['d', 100000])
+		   	    priority.append(['u', 100000])
 				
 		elif [j, height - 1] in list:
 		    # Direction is not up
