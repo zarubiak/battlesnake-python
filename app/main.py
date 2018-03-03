83e5830040d69a59a9262cf5b0b1b591dd76a3e5
Cam Zarubiak <cameronz@telus.net>
2018-03-03 11:03:38 -0800

    online

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def start():
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'battlesnake-python',
-		'head_type': 'shadaes',
+		'head_type': 'shades',
 		'tail_type': 'curled',
 		'secondary_color': '#DDA0DD'
     }
