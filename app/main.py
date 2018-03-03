c9e17678844db65815064cfd22b1c4610ef6989b
Cam Zarubiak <cameronz@telus.net>
2018-03-03 10:17:58 -0800

    online

--- app/main.py
+++ app/main.py
@@ -127,37 +127,37 @@ def move():
     for j in range(0, width):
 		if [j, 0] in list:
 		    #Direction is not down
-		    priority.append(['u', 900]) 
+		    priority.append(['u', 400]) 
 		    #priority.append(['l', 9000])
 		    #priority.append(['r', 9000])
 		    #break
 		    if [j, height - 2] in list:
-		        priority.append(['d', 400])
+		        priority.append(['d', 100])
 				
 		elif [j, height - 1] in list:
 		    # Direction is not up
-		    priority.append(['d', 900])
+		    priority.append(['d', 400])
 		    #priority.append(['l', 9000])
 		    #priority.append(['r', 9000])
 		    if [j, 1] in list:
-		        priority.append(['u', 400])
+		        priority.append(['u', 100])
 				
     for k in range(0, height):
 		if [0, k] in list:
-		    priority.append(['l', 900])
+		    priority.append(['l', 400])
 		    #priority.append(['d', 9000])
 		    #priority.append(['u', 9000])
 		    # Direction is not left
 		    if [j, 1] in list:
-		        priority.append(['d', 400])
+		        priority.append(['d', 100])
 				
 		elif [width - 1, k] in list:
-		    priority.append(['r', 900])
+		    priority.append(['r', 400])
 		    #priority.append(['u', 9000])
 		    #priority.append(['d', 9000])
 		    # Direction is not right
 		    if [j, 1] in list:
-		        priority.append(['u', 400])	        
+		        priority.append(['u', 100])	        
 			
     if (snakehead_x >= food_x and snakehead_y >= food_y):
         # direction = 'left';
